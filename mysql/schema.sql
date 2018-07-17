5724d0d6da4be39deea30d3b1d0df22fc9a3761d
Emile <emile.swarts123@gmail.com>
2018-07-17 13:11:48 +0100

    Add Userdetails table to schema for testing

--- mysql/schema.sql
+++ mysql/schema.sql
@@ -74,6 +74,32 @@ CREATE TABLE `site` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `userdetails`
+--
+
+DROP TABLE IF EXISTS `userdetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userdetails` (
+  `username` varchar(10) NOT NULL DEFAULT '',
+  `contact` varchar(100) DEFAULT NULL,
+  `sponsor` varchar(100) DEFAULT NULL,
+  `password` varchar(64) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
+  `notifications_opt_out` tinyint(1) NOT NULL DEFAULT '0',
+  `survey_opt_out` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `last_login` datetime DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  KEY `userdetails_created_at` (`created_at`),
+  KEY `userdetails_contact` (`contact`),
+  KEY `userdetails_last_login` (`last_login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `siteip`
 --
 
@@ -99,3 +125,4 @@ CREATE TABLE `siteip` (
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2018-04-18 11:33:13sess
+
