2d66acd88767c366b9c9ab57969ff567e149f476
Emile <emile.swarts123@gmail.com>
2018-07-17 13:28:13 +0100

    Accept the rest of the arguments in the URL
    These will be persisted and last_login will be updated if the
    authentication result is Access-Accept
    
    There is some more edge case testing to be done when values are missing.
    The MAC formatter also still needs to be introduced into this object.

--- app.rb
+++ app.rb
@@ -2,13 +2,14 @@ require 'sequel'
 require 'sinatra/base'
 require './lib/mac_formatter.rb'
 require './lib/session.rb'
+require './lib/user.rb'
 
 class App < Sinatra::Base
   get '/healthcheck' do
     'Healthy'
   end
 
-  get '/logging/post-auth/user/:username/mac/:mac/ap/:called_station_id/site/:site_ip_address/*' do
+  get '/logging/post-auth/user/:username/mac/:mac/ap/:called_station_id/site/:site_ip_address/result/:authentication_result' do
     Session.create(
       username: params.fetch(:username),
       mac: params.fetch(:mac),
@@ -16,5 +17,18 @@ class App < Sinatra::Base
       siteIP: params.fetch(:site_ip_address),
       building_identifier: params.fetch(:called_station_id),
     )
+
+    if params.fetch(:authentication_result) == 'Access-Accept'
+      if params.fetch(:username) != 'HEALTH'
+        user = User.find(username: params.fetch(:username))
+        user.last_login = Time.now.strftime('%y-%m-%d %H:%M:%S')
+        user.save
+      end
+      status 204
+    elsif params.fetch(:authentication_result) == 'Access-Reject'
+      status 204
+    else
+      status 404
+    end
   end
 end
--- spec/features/post_auth_spec.rb
+++ spec/features/post_auth_spec.rb
@@ -1,6 +1,7 @@
 describe App do
   before do
     DB[:sessions].truncate
+    DB[:userdetails].truncate
   end
 
   describe 'POST post-auth' do
@@ -8,15 +9,17 @@ describe App do
     let(:mac) { 'da-59-19-8b-39-2d' }
     let(:called_station_id) { '01-39-38-25-2a-80' }
     let(:site_ip_address) { '93.11.238.187' }
+    let(:post_auth_request) { get "/logging/post-auth/user/#{username}/mac/#{mac}/ap/#{called_station_id}/site/#{site_ip_address}/result/#{authentication_result}" }
+    let(:user) { User.find(username: username) }
+    let(:authentication_result) { 'Access-Accept' }
+
+    before do
+      User.create(username: username)
+      post_auth_request
+    end
 
     context 'Access-Accept' do
-      let(:authentication_result) { 'Access-Accept' }
-
       context 'GovWifi user' do
-        before do
-          get "/logging/post-auth/user/#{username}/mac/#{mac}/ap/#{called_station_id}/site/#{site_ip_address}/result/#{authentication_result}"
-        end
-
         it 'creates a single session record' do
           expect(Session.count).to eq(1)
         end
@@ -31,25 +34,57 @@ describe App do
           expect(session.building_identifier).to eq(called_station_id)
         end
 
-        it 'updates the users last login'
-      end
+        context 'user last login' do
+          context 'HEALTH user' do
+            let(:username) { 'HEALTH' }
 
-      context 'HEALTH user' do
-        it 'does not records the authentication request'
-      end
+            it 'does not update the last login' do
+              post_auth_request
+              expect(user.last_login).to be_nil
+            end
+          end
 
-      it 'returns a no-content header'
+          context 'GovWifi user' do
+            it 'does updates the last login' do
+              post_auth_request
+              expect(user.last_login).to_not be_nil
+            end
+          end
+        end
+      end
     end
 
     context 'Access-Reject' do
-      it 'returns a 204 OK'
+      let(:authentication_result) { 'Access-Reject' }
+
+
+      it 'does not record last_login for the user' do
+        post_auth_request
+        expect(user.last_login).to be_nil
+      end
     end
 
-    context 'Unknown' do
-      it 'deals with an unknown result'
+    it 'returns a 204 OK' do
+      post_auth_request
+      expect(last_response.status).to eq(204)
     end
 
-    context 'Invalid URL format' do
+    context 'Unknown authentication result' do
+      let(:authentication_result) { 'unknown' }
+
+      it 'returns a 404 for anything other than Access-Accept or Access-Reject' do
+        post_auth_request
+        expect(last_response.status).to eq(404)
+      end
+    end
+
+    context 'Blank authentication result' do
+      let(:authentication_result) { '' }
+
+      it 'returns a 404 for anything other than Access-Accept or Access-Reject' do
+        post_auth_request
+        expect(last_response.status).to eq(404)
+      end
     end
   end
 end
