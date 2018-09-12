dd85f09786847a17ecad29e720111c7c33fec544
Kyle <kjdchapman@gmail.com>
2018-09-12 12:00:06 +0100

    WIP get s3 content using gem

--- lib/performance_platform/gateway/s3_ip_locations.rb
+++ lib/performance_platform/gateway/s3_ip_locations.rb
@@ -1,4 +1,22 @@
+require 'aws-sdk-s3'
+
 class PerformancePlatform::Gateway::S3IpLocations
-  def fetch
+  def initialize
+    @s3 = Aws::S3::Client.new(config)
   end
+
+  def fetch
+    resp = s3.get_object(bucket: 'bucket-name', key: 'object-key')
+    resp.body.read
+  end
+
+private
+
+    DEFAULT_REGION = 'eu-west-2'.freeze
+
+    def config
+      { region: DEFAULT_REGION }
+    end
+
+  attr_reader :s3
 end
--- spec/features/publish_daily_statistics_spec.rb
+++ spec/features/publish_daily_statistics_spec.rb
@@ -26,18 +26,47 @@ describe 'synchronizing IPs and locations' do
     end
   end
 
-  xcontext 'three IPs and locations are in source' do
+  context 'three IPs and locations are in source' do
+    let(:object_content) do
+      [
+        {
+          ip: '127.0.0.1',
+          location_id: 1
+        }, {
+          ip: '186.3.1.1',
+          location_id: 2
+        }, {
+          ip: '186.3.4.6',
+          location_id: 3
+        }
+      ]
+    end
+
+    let(:bucket) { 'StubBucket' }
+    let(:key) { 'StubKey' }
+
     before do
-      # webmock s3 here?
+      ENV['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI'] = '/stubUri'
+
+      stub_request(:get, 'http://169.254.170.2/stubUri').to_return(body: {
+        'AccessKeyId': 'ACCESS_KEY_ID',
+        'Expiration': (Time.now + 60).iso8601,
+        'RoleArn': 'TASK_ROLE_ARN',
+        'SecretAccessKey': 'SECRET_ACCESS_KEY',
+        'Token': 'SECURITY_TOKEN_STRING'
+      }.to_json)
+
+      stub_request(:get, 'https://s3.eu-west-1.amazonaws.com/#{bucket}/#{key}') \
+        .to_return(body: object_content.to_json)
     end
 
     it 'saves three IPs and locations' do
       subject.execute
-      expect(ip_locations.count).to be_zero
+      expect(ip_locations.count).to eq(3)
     end
 
     context 'with existing IPs and locations in the database' do
-      before { ip_locations.insert(ip: 1, location_id: 1) }
+      before { ip_locations.insert(ip: '186.3.1.1', location_id: 1) }
 
       it 'overwrites them instead of adding to them' do
         subject.execute
