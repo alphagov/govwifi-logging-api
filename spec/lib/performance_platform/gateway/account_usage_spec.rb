070ee5a79dcd9ea8c787f70a076b18e39b615497
Kyle <kjdchapman@gmail.com>
2018-09-17 12:49:06 +0100

    Improve + extend account usage specs

--- spec/lib/performance_platform/gateway/account_usage_spec.rb
+++ spec/lib/performance_platform/gateway/account_usage_spec.rb
@@ -1,18 +1,198 @@
 describe PerformancePlatform::Gateway::AccountUsage do
-  let(:session_repository) { DB[:sessions] }
-  let(:site_repository) { DB[:site] }
-  let(:site_ip_repository) { DB[:siteip] }
+  let(:sessions) { DB[:sessions] }
+  let(:locations) { DB[:site] }
+  let(:location_ip_links) { DB[:siteip] }
 
-  before do
-    DB[:sessions].truncate
-    DB[:siteip].truncate
-    DB[:site].truncate
-  end
+  let(:ip_A1) { '104.24.112.118' }
+  let(:ip_A2) { '104.24.112.120' }
+  let(:ip_B1) { '216.58.201.46' }
+  let(:ip_B2) { '216.58.201.49' }
+
+  let(:result) { subject.fetch_stats }
 
   subject { described_class.new(period: 'week') }
 
-  context 'given no sessions' do
-    it 'returns stats with zero sessions' do
+  before do
+    sessions.truncate
+    locations.truncate
+    location_ip_links.truncate
+
+    location_a = locations.insert(address: 'House One', org_id: 1)
+    location_b = locations.insert(address: 'House Two', org_id: 1)
+
+    location_ip_links.insert(ip: ip_A1, site_id: location_a)
+    location_ip_links.insert(ip: ip_A2, site_id: location_a)
+    location_ip_links.insert(ip: ip_B1, site_id: location_b)
+    location_ip_links.insert(ip: ip_B2, site_id: location_b)
+  end
+
+  describe 'one user' do
+    context 'with one session' do
+      before do
+        sessions.insert(
+          siteIP: ip_A1,
+          username: 'alice',
+          start: Date.today - 1
+        )
+      end
+
+      it 'generates correct stats' do
+        expect(result[:total]).to eq(1)
+        expect(result[:transactions]).to eq(1)
+        expect(result[:roaming]).to eq(0)
+        expect(result[:one_time]).to eq(1)
+      end
+
+      it 'generates correct tags' do
+        expect(result[:metric_name]).to eq('account-usage')
+        expect(result[:period]).to eq('week')
+      end
+    end
+
+    context 'with two sessions' do
+      before do
+        sessions.insert(
+          siteIP: first_ip,
+          username: 'alice',
+          start: Date.today - 1
+        )
+
+        sessions.insert(
+          siteIP: second_ip,
+          username: 'alice',
+          start: Date.today - 1
+        )
+      end
+
+      context 'on the same IP' do
+        let(:first_ip) { ip_A1 }
+        let(:second_ip) { first_ip }
+
+        it 'generates correct stats' do
+          expect(result[:total]).to eq(1)
+          expect(result[:transactions]).to eq(1)
+          expect(result[:roaming]).to eq(0)
+          expect(result[:one_time]).to eq(1)
+        end
+      end
+
+      context 'on different IPs within the same location' do
+        let(:first_ip) { ip_A1 }
+        let(:second_ip) { ip_A2 }
+
+        it 'generates correct stats' do
+          expect(result[:total]).to eq(1)
+          expect(result[:transactions]).to eq(1)
+          expect(result[:roaming]).to eq(0)
+          expect(result[:one_time]).to eq(1)
+        end
+      end
+
+      context 'on different IPs across different locations' do
+        let(:first_ip) { ip_A1 }
+        let(:second_ip) { ip_B1 }
+
+        it 'generates correct stats' do
+          expect(result[:total]).to eq(1)
+          expect(result[:transactions]).to eq(2)
+          expect(result[:roaming]).to eq(1)
+          expect(result[:one_time]).to eq(0)
+        end
+      end
+    end
+  end
+
+  describe 'two users' do
+    context 'neither roaming' do
+      before do
+        sessions.insert(
+          siteIP: ip_A1,
+          username: 'alice',
+          start: Date.today - 1
+        )
+
+        sessions.insert(
+          siteIP: ip_A2,
+          username: 'bob',
+          start: Date.today - 1
+        )
+      end
+
+      it 'generates correct stats' do
+        expect(result[:total]).to eq(2)
+        expect(result[:transactions]).to eq(2)
+        expect(result[:roaming]).to eq(0)
+        expect(result[:one_time]).to eq(2)
+      end
+    end
+
+    context 'one roaming, one not' do
+      before do
+        sessions.insert(
+          siteIP: ip_A1,
+          username: 'alice',
+          start: Date.today - 1
+        )
+
+        sessions.insert(
+          siteIP: ip_B1,
+          username: 'alice',
+          start: Date.today - 1
+        )
+
+        sessions.insert(
+          siteIP: ip_A2,
+          username: 'bob',
+          start: Date.today - 1
+        )
+      end
+
+      it 'generates correct stats' do
+        expect(result[:total]).to eq(2)
+        expect(result[:transactions]).to eq(3)
+        expect(result[:roaming]).to eq(1)
+        expect(result[:one_time]).to eq(1)
+      end
+    end
+
+    context 'both roaming' do
+      before do
+        sessions.insert(
+          siteIP: ip_A1,
+          username: 'alice',
+          start: Date.today - 1
+        )
+
+        sessions.insert(
+          siteIP: ip_B1,
+          username: 'alice',
+          start: Date.today - 1
+        )
+
+        sessions.insert(
+          siteIP: ip_A2,
+          username: 'bob',
+          start: Date.today - 1
+        )
+
+        sessions.insert(
+          siteIP: ip_B2,
+          username: 'bob',
+          start: Date.today - 1
+        )
+      end
+
+      it 'generates correct stats' do
+        expect(result[:total]).to eq(2)
+        expect(result[:transactions]).to eq(4)
+        expect(result[:roaming]).to eq(2)
+        expect(result[:one_time]).to eq(0)
+      end
+    end
+  end
+
+  context 'zero sessions' do
+    it 'generates the correct (empty) stats' do
       expect(subject.fetch_stats).to eq(
         total: 0,
         transactions: 0,
@@ -24,71 +204,21 @@ describe PerformancePlatform::Gateway::AccountUsage do
     end
   end
 
-  context 'given many sessions' do
+  context 'a session two days ago' do
     before do
-      session_repository.insert(
-        siteIP: '127.0.0.1',
+      sessions.insert(
+        siteIP: ip_A1,
         username: 'bob',
-        start: Date.today - 1
-      )
-
-      session_repository.insert(
-        siteIP: '127.0.0.9',
-        username: 'bob',
-        start: Date.today - 1
-      )
-
-      session_repository.insert(
-        siteIP: '127.0.0.1',
-        username: 'bob',
-        start: Date.today - 1
-      )
-
-      session_repository.insert(
-        siteIP: '127.0.0.9',
-        username: 'alice',
-        start: Date.today - 1
-      )
-
-      session_repository.insert(
-        siteIP: '127.0.0.9',
-        username: 'alice',
-        start: Date.today - 1
-      )
-
-      session_repository.insert(
-        siteIP: '127.0.0.9',
-        username: 'george',
         start: Date.today - 2
-      )
-
-      site1_id = site_repository.insert(
-        address: 'hello',
-        org_id: 1
-      )
-
-      site2_id = site_repository.insert(
-        address: 'foo',
-        org_id: 2
-      )
-
-      site_ip_repository.insert(
-        ip: '127.0.0.1',
-        site_id: site1_id
-      )
-
-      site_ip_repository.insert(
-        ip: '127.0.0.9',
-        site_id: site2_id
       )
     end
 
-    it 'returns stats for sessions' do
+    it 'generates the correct (empty) stats' do
       expect(subject.fetch_stats).to eq(
-        total: 2,
-        transactions: 3,
-        roaming: 1,
-        one_time: 1,
+        total: 0,
+        transactions: 0,
+        roaming: 0,
+        one_time: 0,
         metric_name: 'account-usage',
         period: 'week',
       )
