7f9beb7dd1e685d4cba713dcd02decb7a5d85e83
Kyle <kjdchapman@gmail.com>
2018-09-17 13:07:20 +0100

    Change source table for roaming 'account usage'
    This commit changes the expected data source for ip/location links
    when calculating 'account usage', from 'site_ip' and 'site' to
    just 'ip_locations', updating the specs to set up this new data.
    
    It also updates the related query so that these tests pass.

--- lib/performance_platform/repository/session.rb
+++ lib/performance_platform/repository/session.rb
@@ -2,17 +2,19 @@ class PerformancePlatform::Repository::Session < Sequel::Model(:sessions)
   dataset_module do
     def account_usage_stats(*)
       DB.fetch("
-       SELECT
-         count(distinct(username)) as total,
-         count(distinct(concat_ws('-', sessions.username, site.address))) as per_site
-         FROM sessions
-         LEFT JOIN siteip
-         ON (siteip.ip = sessions.siteIP)
-         LEFT JOIN site
-         ON (siteip.site_id = site.id)
-           WHERE site.org_id IS NOT NULL
-           AND date(sessions.start) = '#{Date.today - 1}'
-      GROUP BY date(start)").first
+        SELECT
+          count(distinct(username)) as total,
+          count(distinct(concat_ws('-', sessions.username, ip_locations.location_id))) as per_site
+        FROM
+          sessions
+          LEFT JOIN
+            ip_locations
+            ON
+            (ip_locations.ip = sessions.siteIP)
+        WHERE
+          date(sessions.start) = '#{Date.today - 1}'
+        GROUP BY
+            date(start)").first
     end
 
     def unique_users_stats(period:)
