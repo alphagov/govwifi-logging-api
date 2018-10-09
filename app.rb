8ea2a1a8029f55c2000eed2fdb8a6465910d7b3a
Craig J. Bass <craig@madetech.com>
2018-10-09 10:43:42 +0100

    Search authentication events by username endpoint (#58)
    Add search endpoint to allow the admin app to query authentication requests for a given username.

--- app.rb
+++ app.rb
@@ -1,14 +1,26 @@
+# frozen_string_literal: true
+
 require 'sequel'
 require 'sinatra/base'
+require 'sinatra/json'
 require './lib/loader'
 
 class App < Sinatra::Base
   configure :production, :staging, :development do
     enable :logging
+    enable :json
   end
 
   get '/healthcheck' do
     'Healthy'
+  end
+
+  get '/authentication/events/search/:username' do
+    username = params[:username]
+    two_weeks_ago = Time.now - 2 * 7 * 24 * 60 * 60
+    sessions = Session.where(username: username).where { start > two_weeks_ago }
+
+    json sessions.map(&:to_hash)
   end
 
   get '/logging/post-auth/user/?:username?/mac/?:mac?/ap/?:called_station_id?/site/?:site_ip_address?/result/:authentication_result' do
