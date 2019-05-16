74d70dbc804baaecb5bee8aff6b83986ffb96cfb
Tom Taylor <tommotaylor@gmail.com>
2019-05-16 11:27:18 +0100

    Gateway for active users

--- lib/performance_platform/repository/session.rb
+++ lib/performance_platform/repository/session.rb
@@ -1,21 +1,11 @@
 class PerformancePlatform::Repository::Session < Sequel::Model(:sessions)
   dataset_module do
-    def account_usage_stats(date)
+    def active_users_stats(period:, date:)
       DB.fetch("
         SELECT
-          count(distinct(username)) as total,
-          count(distinct(concat_ws('-', sessions.username, ip_locations.location_id))) as per_site
-        FROM
-          sessions
-          LEFT JOIN
-            ip_locations
-            ON
-            (ip_locations.ip = sessions.siteIP)
-        WHERE
-          date(sessions.start) = '#{date - 1}'
-          AND sessions.success = 1
-        GROUP BY
-            date(start)").first
+          count(distinct(username)) as total
+        FROM sessions WHERE start BETWEEN date_sub('#{date-1}', INTERVAL 1 #{period}) AND '#{date-1}'
+          AND sessions.success = 1").first
     end
 
     def unique_users_stats(period:, date:)
