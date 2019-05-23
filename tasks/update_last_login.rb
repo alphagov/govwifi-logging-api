a0f1f1ff38b94cab86833f86ed47d8ce4c8ca179
Anthony King <anthony@madetech.com>
2019-05-23 12:13:59 +0100

    point at GDPR, remove support for custom dates

--- tasks/update_last_login.rb
+++ tasks/update_last_login.rb
@@ -1,18 +1,8 @@
 require 'date'
 
 task :update_yesterdays_last_login do
-  LastLoginSync::UseCase::PopulateUserLastLogin.new(
-    active_users_gateway: LastLoginSync::Gateway::Activity.new,
-    last_login_gateway: LastLoginSync::Gateway::SetLastLogin.new
+  Gdpr::UseCase::PopulateUserLastLogin.new(
+      session_gateway: Gdpr::Gateway::Session.new,
+      last_login_gateway: Gdpr::Gateway::SetLastLogin.new
   ).execute(date: Date.today.prev_day)
-end
-
-task :update_last_login, [:date] do |_, args|
-  args.with_defaults(date: Date.today.to_s)
-  date = Date.parse(args[:date])
-
-  LastLoginSync::UseCase::PopulateUserLastLogin.new(
-    active_users_gateway: LastLoginSync::Gateway::Activity.new,
-    last_login_gateway: LastLoginSync::Gateway::SetLastLogin.new
-  ).execute(date: date)
 end
