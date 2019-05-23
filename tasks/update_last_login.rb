07fb388935ceb9862598c2a4e22c42087db4354c
Anthony King <anthony@madetech.com>
2019-05-23 11:11:31 +0100

    restructure task to be cleaner
    no more special args, just use another task

--- tasks/update_last_login.rb
+++ tasks/update_last_login.rb
@@ -1,13 +1,15 @@
 require 'date'
 
+task :update_yesterdays_last_login do
+  LastLoginSync::UseCase::PopulateUserLastLogin.new(
+    active_users_gateway: LastLoginSync::Gateway::Activity.new,
+    last_login_gateway: LastLoginSync::Gateway::SetLastLogin.new
+  ).execute(date: Date.today.prev_day)
+end
+
 task :update_last_login, [:date] do |_, args|
   args.with_defaults(date: Date.today.to_s)
-
-  date = if args[:date] == 'yesterday'
-           Date.today.prev_day
-         else
-           args[:date].to_date
-         end
+  date = Date.parse(args[:date])
 
   LastLoginSync::UseCase::PopulateUserLastLogin.new(
     active_users_gateway: LastLoginSync::Gateway::Activity.new,
