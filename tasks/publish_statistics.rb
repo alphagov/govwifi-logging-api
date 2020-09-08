136092c44e69022a721c68015e16750abee97ab2
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2020-09-08 16:42:47 +0100

    Add the roaming_users data points to the S3 Bucket
    Data points about roaming users that are normally sent to the PP
    are now also sent to an S3 bucket.

--- tasks/publish_statistics.rb
+++ tasks/publish_statistics.rb
@@ -50,12 +50,15 @@ PERIODS.each do |adverbial, period|
 
     logger.info("Creating #{adverbial} metrics for S3 with #{args[:date]}")
 
-    metrics = Metrics::ActiveUsers.new(period: period, date: args[:date])
+    metrics_list = [Metrics::ActiveUsers.new(period: period, date: args[:date]),
+                    Metrics::RoamingUsers.new(period: period, date: args[:date])]
 
-    logger.info("[#{metrics.key}] Fetching and uploading metrics...")
+    metrics_list.each do |metrics|
+      logger.info("[#{metrics.key}] Fetching and uploading metrics...")
 
-    metrics.execute
+      metrics.execute
 
-    logger.info("[#{metrics.key}] Done.")
+      logger.info("[#{metrics.key}] Done.")
+    end
   end
 end
