6d977312649aefad3a0312cd2fee4cbbafb6bd2b
Colin Burn-Murdoch <colin.burn-murdoch@digital.cabinet-office.gov.uk>
2021-04-19 08:10:17 -0400

    Elasticsearch Gateway

---
+++ spec/lib/volumetrics/gateway/elasticsearch_spec.rb
@@ -0,0 +1,20 @@
+describe Volumetrics::Gateway::Elasticsearch, :focus do
+  let(:elasticsearch_client) { double(index: nil) }
+  let(:url) { "http://#{ENV['VOLUMETRICS_ENDPOINT']}:9200/volumetrics/object/bar" }
+
+  before do
+    ENV["VOLUMETRICS_ENDPOINT"] = "foo"
+
+    stub_request(:put, url).with(
+      body: { foo: "bar" }.to_json,
+    ).to_return(status: 200)
+  end
+
+  it "calls ElasticSearch API with expected args" do
+    subject.write("bar", { foo: "bar" })
+
+    assert_requested :put, url,
+                     body: { foo: "bar" }.to_json,
+                     times: 1
+  end
+end
