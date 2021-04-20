846d762d1d9d35a54e84faebf7f6516cea3500f6
Colin Burn-Murdoch <colin.burn-murdoch@digital.cabinet-office.gov.uk>
2021-04-20 06:44:45 -0400

    Strip prefix off key when returning from S3 Gateway

--- lib/volumetrics/gateway/s3.rb
+++ lib/volumetrics/gateway/s3.rb
@@ -1,10 +1,12 @@
 class Volumetrics::Gateway::S3
   include Enumerable
 
+  PREFIX = "volumetrics/".freeze
+
   def each(&block)
     keys.each do |key|
       json = Services.s3_client.get_object(bucket: bucket, key: key)
-      block.call({ key: key, body: JSON.parse(json.body.read) })
+      block.call({ key: key[PREFIX.length..-1], body: JSON.parse(json.body.read) })
     end
   end
 
@@ -15,7 +17,7 @@ private
   end
 
   def list_objects(continuation_token = nil)
-    response = Services.s3_client.list_objects_v2({ bucket: bucket, prefix: "volumetrics/", continuation_token: continuation_token })
+    response = Services.s3_client.list_objects_v2({ bucket: bucket, prefix: PREFIX, continuation_token: continuation_token })
     objects = response.data.contents
 
     if response.data.is_truncated
