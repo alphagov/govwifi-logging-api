01da942d93bdc1d62d5f08763fecf5c08bd4b4b0
Colin Burn-Murdoch <colin.burn-murdoch@digital.cabinet-office.gov.uk>
2021-05-05 07:50:37 -0400

    Update S3 class to take prefix parameter

--- spec/lib/performance/gateway/s3_spec.rb
+++ spec/lib/performance/gateway/s3_spec.rb
@@ -2,6 +2,7 @@ require_relative "../../metrics/s3_fake_client"
 
 describe Performance::Gateway::S3 do
   let(:s3_client) { Metrics.fake_s3_client }
+  let(:subject) { described_class.new("volumetrics") }
 
   before do
     ENV["S3_METRICS_BUCKET"] = "stub-bucket"
--- tasks/sync_s3_volumetrics.rb
+++ tasks/sync_s3_volumetrics.rb
@@ -1,7 +1,7 @@
 task :sync_s3_volumetrics do
   Performance::UseCase::SyncS3ToElasticsearch.new(
     elasticsearch_gateway: Performance::Gateway::Elasticsearch.new,
-    s3_gateway: Performance::Gateway::S3.new,
+    s3_gateway: Performance::Gateway::S3.new("volumetrics"),
   ).execute
 end
 
