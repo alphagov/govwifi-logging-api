256831b18b8a09a1793efb1e3221f86634640cc3
Colin Burn-Murdoch <colin.burn-murdoch@digital.cabinet-office.gov.uk>
2021-05-05 11:46:13 -0400

    Call the updated sync task for all metrics.

--- tasks/sync_s3_volumetrics.rb
+++ tasks/sync_s3_volumetrics.rb
@@ -3,5 +3,19 @@ task :sync_s3_volumetrics do
     elasticsearch_gateway: Performance::Gateway::Elasticsearch.new("volumetrics"),
     s3_gateway: Performance::Gateway::S3.new("volumetrics"),
   ).execute
-end
 
+  Performance::UseCase::SyncS3ToElasticsearch.new(
+    elasticsearch_gateway: Performance::Gateway::Elasticsearch.new("active-users"),
+    s3_gateway: Performance::Gateway::S3.new("active-users"),
+  ).execute
+
+  Performance::UseCase::SyncS3ToElasticsearch.new(
+    elasticsearch_gateway: Performance::Gateway::Elasticsearch.new("roaming-users"),
+    s3_gateway: Performance::Gateway::S3.new("roaming-users"),
+  ).execute
+
+  Performance::UseCase::SyncS3ToElasticsearch.new(
+    elasticsearch_gateway: Performance::Gateway::Elasticsearch.new("completion_rate"),
+    s3_gateway: Performance::Gateway::S3.new("completion_rate"),
+  ).execute
+end
