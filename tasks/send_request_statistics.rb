d86e6352870c6f2f8e7217323fa09b093fbd5634
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2021-08-19 15:09:07 +0100

    Make send_request_statistics rake task load the environment
    Otherwise it can't find any of the classes.

--- tasks/send_request_statistics.rb
+++ tasks/send_request_statistics.rb
@@ -1,6 +1,5 @@
 require "logger"
-require "./lib/performance/metrics"
 
-task :send_request_statistics do
+task send_request_statistics: :load_env do
   Performance::Metrics::RequestStatsSender.new.send_data
 end
