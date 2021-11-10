93bb30e6db81c79bcf911f67b04ab39dd1935123
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2021-11-10 12:51:13 +0000

    Refactor: avoid repetition in sync_s3_volumetrics task

--- tasks/sync_s3_volumetrics.rb
+++ tasks/sync_s3_volumetrics.rb
@@ -1,23 +1,10 @@
 task sync_s3_volumetrics: :load_env do
   bucket = ENV.fetch("S3_METRICS_BUCKET")
 
-  Performance::UseCase::SyncS3ToElasticsearch.new(
-    elasticsearch_gateway: Performance::Gateway::Elasticsearch.new(Performance::Metrics::ELASTICSEARCH_INDEX),
-    s3_gateway: Performance::Gateway::S3.new("volumetrics", bucket),
-  ).execute
-
-  Performance::UseCase::SyncS3ToElasticsearch.new(
-    elasticsearch_gateway: Performance::Gateway::Elasticsearch.new(Performance::Metrics::ELASTICSEARCH_INDEX),
-    s3_gateway: Performance::Gateway::S3.new("active_users", bucket),
-  ).execute
-
-  Performance::UseCase::SyncS3ToElasticsearch.new(
-    elasticsearch_gateway: Performance::Gateway::Elasticsearch.new(Performance::Metrics::ELASTICSEARCH_INDEX),
-    s3_gateway: Performance::Gateway::S3.new("roaming_users", bucket),
-  ).execute
-
-  Performance::UseCase::SyncS3ToElasticsearch.new(
-    elasticsearch_gateway: Performance::Gateway::Elasticsearch.new(Performance::Metrics::ELASTICSEARCH_INDEX),
-    s3_gateway: Performance::Gateway::S3.new("completion_rate", bucket),
-  ).execute
+  %w[volumetrics active_users roaming_users completion_rate].map do |metric|
+    Performance::UseCase::SyncS3ToElasticsearch.new(
+      elasticsearch_gateway: Performance::Gateway::Elasticsearch.new(Performance::Metrics::ELASTICSEARCH_INDEX),
+      s3_gateway: Performance::Gateway::S3.new(metric, bucket),
+    ).execute
+  end
 end
