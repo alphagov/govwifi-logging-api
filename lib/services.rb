de24fb7f89b778bed9ba92f16580fb8db1218eb9
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2021-11-17 13:25:00 +0000

    Memoize S3 and Elasticsearch client
    To ensure we do not instantiate this multiple times.

--- lib/services.rb
+++ lib/services.rb
@@ -1,9 +1,9 @@
 class Services
   def self.s3_client
-    Aws::S3::Client.new(region: "eu-west-2")
+    @s3_client ||= Aws::S3::Client.new(region: "eu-west-2")
   end
 
   def self.elasticsearch_client
-    Elasticsearch::Client.new host: ENV["VOLUMETRICS_ENDPOINT"]
+    @elasticsearch_client ||= Elasticsearch::Client.new host: ENV["VOLUMETRICS_ENDPOINT"]
   end
 end
