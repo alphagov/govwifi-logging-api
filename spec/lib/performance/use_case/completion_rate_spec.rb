df9a157012bc66f6d2d42310593116e9805de22d
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2022-05-19 17:00:18 +0100

    Add cumulative metrics to completion rate metrics
    We want to know how many users have signed up over 3 journeys. We
    also want to know if they managed to sign in

--- spec/lib/performance/use_case/completion_rate_spec.rb
+++ spec/lib/performance/use_case/completion_rate_spec.rb
@@ -4,85 +4,113 @@ describe Performance::UseCase::CompletionRate do
   before do
     USER_DB[:userdetails].truncate
 
-    # Outside of date scope
+    # SMS self-registered outside of date scope, not logged in
     user_repo.create(
       username: "1",
-      created_at: today - 1,
+      created_at: today - 10,
       contact: "+1234567890",
       sponsor: "+1234567890",
+      last_login: nil,
     )
 
-    # Outside of date scope
-    # and logged in
+    # SMS self-registered outside of date scope and logged in
     user_repo.create(
       username: "2",
-      created_at: today - 1,
+      created_at: today - 10,
       contact: "+1234567890",
       sponsor: "+1234567890",
       last_login: today,
     )
 
-    # SMS self-registered within date scope
+    # SMS self-registered within date scope not logged in
     user_repo.create(
       username: "3",
-      created_at: today - 8,
+      created_at: today - 5,
       contact: "+2345678901",
       sponsor: "+2345678901",
+      last_login: nil,
     )
 
     # SMS self-registered within date scope
     # and logged in
     user_repo.create(
       username: "4",
-      created_at: today - 8,
+      created_at: today - 5,
       contact: "+2345678901",
       sponsor: "+2345678901",
       last_login: today,
     )
 
-    # SMS sponsor-registered within date scope
+    # SMS sponsor-registered outside date scope not logged in
     user_repo.create(
       username: "5",
-      created_at: today - 8,
+      created_at: today - 10,
       contact: "+2345678901",
       sponsor: "sponsor@example.com",
+      last_login: nil,
     )
 
-    # SMS sponsor-registered within date scope
-    # and logged in
+    # SMS sponsor-registered outside date scope logged in
     user_repo.create(
       username: "6",
-      created_at: today - 8,
+      created_at: today - 10,
       contact: "+2345678901",
       sponsor: "sponsor@example.com",
       last_login: today,
     )
 
-    # email self-registered within scope
+    # SMS sponsor-registered within date scope not logged in
     user_repo.create(
       username: "7",
-      created_at: today - 10,
-      contact: "me@example.com",
-      sponsor: "me@example.com",
+      created_at: today - 5,
+      contact: "+2345678901",
+      sponsor: "sponsor@example.com",
+      last_login: nil,
     )
 
-    # Email self-registered within scope
+    # SMS sponsor-registered within date scope
     # and logged in
     user_repo.create(
       username: "8",
-      created_at: today - 10,
-      contact: "me@example.com",
-      sponsor: "me@example.com",
+      created_at: today - 5,
+      contact: "+2345678901",
+      sponsor: "sponsor@example.com",
       last_login: today,
     )
 
-    # Email sponsored
-    # and logged in
+    # email self-registered outside scope not logged in
     user_repo.create(
       username: "9",
       created_at: today - 10,
       contact: "me@example.com",
-      sponsor: "sponsor@example.com",
+      sponsor: "me@example.com",
+      last_login: nil,
+    )
+
+    # Email self-registered outside scope and logged in
+    user_repo.create(
+      username: "10",
+      created_at: today - 10,
+      contact: "me@example.com",
+      sponsor: "me@example.com",
+      last_login: today,
+    )
+
+    # email self-registered within scope not logged in
+    user_repo.create(
+      username: "11",
+      created_at: today - 5,
+      contact: "me@example.com",
+      sponsor: "me@example.com",
+      last_login: nil,
+    )
+
+    # Email self-registered within scope and logged in
+    user_repo.create(
+      username: "12",
+      created_at: today - 5,
+      contact: "me@example.com",
+      sponsor: "me@example.com",
       last_login: today,
     )
   end
@@ -92,12 +120,22 @@ describe Performance::UseCase::CompletionRate do
       expect(subject.fetch_stats).to eq(
         metric_name: "completion-rate",
         period: "week",
+        all_registered: 6,
+        all_logged_in: 3,
         sms_registered: 2,
         sms_logged_in: 1,
         email_registered: 2,
         email_logged_in: 1,
-        sponsor_registered: 3,
-        sponsor_logged_in: 2,
+        sponsor_registered: 2,
+        sponsor_logged_in: 1,
+        cumulative_all_registered: 12,
+        cumulative_all_logged_in: 6,
+        cumulative_sms_registered: 4,
+        cumulative_sms_logged_in: 2,
+        cumulative_email_registered: 4,
+        cumulative_email_logged_in: 2,
+        cumulative_sponsor_registered: 4,
+        cumulative_sponsor_logged_in: 2,
         date: today.to_s,
       )
     end
