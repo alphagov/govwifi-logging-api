0c02ba4d11cf426c3f9484afa1810527114d062e
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2022-05-30 12:17:23 +0100

    Update active_users query to not use the 'Date' function
    Running the query previously proved to be extremely slow. This is
    because the Sessions table contains > 100 million rows and the query
    used the "Date" function to select rows with a start attribute within
    the specified day. This means that for each of the >100 million rows
    the date function is applied which also makes it impossible for the
    database to use the relevant index.
    
    The change removes the 'Date' function from the query and instead
    performs a direct comparison.
    
    An extra test was introduced to ensure we select the correct Session
    rows.

--- spec/lib/gdpr/use_case/populate_user_last_login_spec.rb
+++ spec/lib/gdpr/use_case/populate_user_last_login_spec.rb
@@ -20,6 +20,24 @@ describe Gdpr::UseCase::PopulateUserLastLogin do
     userdetails.truncate
   end
 
+  it "only uses sessions with a start date within the day specified" do
+    sessions.insert(username: "AAAAAA", start: "2020-03-09 23:59:59")
+    sessions.insert(username: "BBBBBB", start: "2020-03-10 00:00:00")
+    sessions.insert(username: "CCCCCC", start: "2020-03-10 23:59:59")
+    sessions.insert(username: "DDDDDD", start: "2020-03-11 00:00:00")
+    userdetails.insert(username: "AAAAAA", last_login: nil)
+    userdetails.insert(username: "BBBBBB", last_login: nil)
+    userdetails.insert(username: "CCCCCC", last_login: nil)
+    userdetails.insert(username: "DDDDDD", last_login: nil)
+
+    subject.execute(date: Date.parse("2020-03-10"))
+
+    expect(userdetails.first(username: "AAAAAA")[:last_login]).to be_nil
+    expect(userdetails.first(username: "BBBBBB")[:last_login]).to eq(Time.parse("2020-03-10"))
+    expect(userdetails.first(username: "CCCCCC")[:last_login]).to eq(Time.parse("2020-03-10"))
+    expect(userdetails.first(username: "DDDDDD")[:last_login]).to be_nil
+  end
+
   context "with no user sessions" do
     it "Does not fail" do
       expect { subject.execute(date: today) }.not_to raise_error
