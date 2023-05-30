a7e7332c9bca726cfdeaf1f2b33c9d9a48a34161
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2023-05-30 20:42:02 +0100

    Update the smoketest cleanup code
    The smoke tests have recently changed. They now take care of
    removing users that have been generated during the tests.
    
    The Session data however still has to be removed. This is now
    done by siteIP and a new environment variable is introduced
    'SMOKE_TEST_IPS' that lists all the site ips that the smoke tests
    use so that the correct sessions will be deleted.

--- spec/lib/smoketests/smoketest_cleanup_spec.rb
+++ spec/lib/smoketests/smoketest_cleanup_spec.rb
@@ -1,95 +1,26 @@
 describe Smoketests::SmoketestCleanup do
   before do
-    User.truncate
     Session.truncate
   end
-
-  context "Deleting smoke test users" do
-    context "Given no test users" do
-      before do
-        User.insert(username: "foo", contact: "foo@example.com", created_at: Date.today - 1)
-        User.insert(username: "bar", contact: "bar@digital.cabinet-office.gov.uk", created_at: Date.today - 1)
-      end
-
-      it "does not delete any users" do
-        expect { subject.clean }.not_to(change { User.count })
-      end
-
-      it "does not delete any sessions" do
-        expect { subject.clean }.not_to(change { Session.count })
-      end
-    end
-
-    context "Given one test user" do
-      before do
-        User.insert(username: "foo", contact: "foo@example.com", created_at: Date.today - 1)
-        User.insert(username: "bar", contact: "bar@digital.cabinet-office.gov.uk", created_at: Date.today - 1)
-        User.insert(username: "baz", contact: "govwifi-tests+baz@digital.cabinet-office.gov.uk", created_at: Date.today - 1)
-        Session.insert(username: "foo")
-        Session.insert(username: "bar")
-        Session.insert(username: "baz")
-      end
-
-      it "deletes only the old test user record" do
-        subject.clean
-        expect(User.select_map(:username)).to match_array(%w[foo bar])
-      end
-
-      it "deletes only associated sessions" do
-        subject.clean
-        expect(Session.select_map(:username)).to match_array(%w[foo bar])
-      end
-    end
-
-    context "Given multiple test users" do
-      before do
-        User.insert(username: "foo", contact: "govwifi-tests+foo@digital.cabinet-office.gov.uk", created_at: Date.today - 1)
-        User.insert(username: "bar", contact: "govwifi-tests+bar@digital.cabinet-office.gov.uk", created_at: Date.today - 1)
-        User.insert(username: "baz", contact: "govwifi-tests+baz@digital.cabinet-office.gov.uk", created_at: Date.today - 1)
-        Session.insert(username: "foo")
-        Session.insert(username: "bar")
-        Session.insert(username: "baz")
-      end
-
-      it "deletes all the test users" do
-        expect { subject.clean }.to change { User.count }.from(3).to(0)
-      end
-      it "deletes all test sessions" do
-        expect { subject.clean }.to change { Session.count }.from(3).to(0)
-      end
-    end
-
-    context "Given a recently created test user" do
-      before do
-        User.insert(username: "foo", contact: "govwifi-tests+foo@digital.cabinet-office.gov.uk", created_at: Date.today - 1)
-        User.insert(username: "bar", contact: "govwifi-tests+bar@digital.cabinet-office.gov.uk", created_at: Date.today - 1)
-        User.insert(username: "baz", contact: "govwifi-tests+baz@digital.cabinet-office.gov.uk")
-        Session.insert(username: "foo")
-        Session.insert(username: "bar")
-        Session.insert(username: "baz")
-      end
-
-      it "does not delete the recent test user" do
-        subject.clean
-        expect(User.select_map(:username)).to include("baz")
-      end
-    end
-    context "More sessions and username than the batch size" do
-      let(:number) { (Smoketests::SmoketestCleanup::SESSION_BATCH_SIZE * 2.5).round }
-      before :each do
-        username = "aaaaaa"
-        number.times do
-          username = username.next
-          User.insert(username:, contact: "govwifi-tests+baz@digital.cabinet-office.gov.uk", created_at: Date.today - 1)
-          2.times { Session.insert(username:) }
-        end
-      end
-      it "deletes all the test users" do
-        expect { subject.clean }.to change { User.count }.from(number).to(0)
-      end
-      it "deletes all test sessions" do
-        expect { subject.clean }.to change { Session.count }.from(number * 2).to(0)
-      end
-    end
+  before :each do
+    10.times { Session.create(siteIP: "1.2.3.4") }
+    10.times { Session.create(siteIP: "2.3.4.5") }
+  end
+  it "deletes all sessions" do
+    ENV["SMOKE_TEST_IPS"] = "1.2.3.4,2.3.4.5"
+    expect { Smoketests::SmoketestCleanup.new.clean }.to change { Session.count }.from(20).to(0)
+  end
+  it "ignores sessions from other IPs" do
+    ENV["SMOKE_TEST_IPS"] = "1.2.3.4"
+    expect { Smoketests::SmoketestCleanup.new.clean }.to change { Session.count }.from(20).to(10)
+    expect(Session.where(siteIP: "2.3.4.5").count).to eq(10)
+  end
+  it "ignores whitespace in the list of IPs" do
+    ENV["SMOKE_TEST_IPS"] = "  1.2.3.4  ,   2.3.4.5   "
+    expect { Smoketests::SmoketestCleanup.new.clean }.to change { Session.count }.from(20).to(0)
+  end
+  it "reports the number of rows deleted" do
+    ENV["SMOKE_TEST_IPS"] = "1.2.3.4"
+    expect { Smoketests::SmoketestCleanup.new.clean }.to output(/10 rows affected/).to_stdout
   end
 end
